3ef4012b34835077873679f5c71fc883154c8e4b
Kais <kais.luce-laurent@efrei.net>
2023-12-10 23:41:24 +0100

    ajout agenda.h

--- main.c
+++ main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
-#include "list.h"
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <locale.h>
 
+
+
+#include "cell.h"
+#include "list.h"
+#include "timer.h"
+#include "agenda.h
+
+int main(){
+    setlocale(LC_ALL, "fr_FR.UTF-8");
+    t_agenda *agenda = creerAgenda();
+    t_recherchecontact search;
+    char *nom;
+    char *prenom;
+    int jour, mois, annee, heure, minutes, heure2, minutes2;
+    char *objet;
+    t_contact *contact;
+    t_rendezvous *rdv;
+    int choix;
+    char c, t[60], t1[60];
+    char date[10], time[5], chx[5];
+    char aff=' ';
+    int nbc, nbr;
+    return 0;
+}
+
+
+/*partie 1 et 2
 int main() {
     t_d_list *list = create_list(5);
     fill_list(list);
@@ -9,3 +40,4 @@ int main() {
     printf("%d", dicho_search(*list, 9));
     return 0;
 }
+*/
